07d7f2b3ed67ab1d50e42242d75df6b3b1284e0d
varad-comrad <fabricio_asfora_002@hotmail.com>
2024-04-30 21:53:10 -0300

    intelligently decided to use langchain

--- wavex/src/main.py
+++ wavex/src/main.py
@@ -4,18 +4,19 @@ import argparse
 import pathlib
 from pprint import pprint
 
-config = {
-    'language': 'en',
-}
-
 class Wavex:
-    def __init__(self, summarizer: Summarizer, transcriber: WhisperTranscriber) -> None:
-        self.transcriber = transcriber
-        # self.summarizer = summarizer
+    def __init__(self, configs: dict) -> None:
+        self.configs = configs
+        self.transcriber = WhisperTranscriber(configs.get('language', 'en'), configs.get('chunk_size', 30))        
 
     def summarize_from_audio(self, audio) -> str:
         transcription = self.transcriber.transcribe(audio)
-        # pprint(transcription)
+        if not hasattr(self, 'summarizer'):
+            if self.configs.get('max_length', None) is None:
+                self.configs['max_length'] = 1000
+            if self.configs.get('min_length', None) is None:
+                self.configs['min_length'] = self.configs['max_length'] // 3
+            self.summarizer = Summarizer(self.configs)
         return self.summarizer.generate(transcription)
 
 def retrieve_args():
@@ -27,9 +28,13 @@ def retrieve_args():
 def main():
     args = retrieve_args()
     language = args.language
+    config = {
+        'language': language,
+        'max_length': 512
+    }
     audios = args.args
     rets = []
-    wavex = Wavex(Summarizer(config), WhisperTranscriber(language))
+    wavex = Wavex(config)
     for audio in audios:
         rets.append(wavex.summarize_from_audio(pathlib.Path(audio)))
     pprint(rets)
--- wavex/src/summarizer.py
+++ wavex/src/summarizer.py
@@ -1,20 +1,33 @@
 import transformers
 import torch
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.llms.huggingface_pipeline import HuggingFacePipeline
+
 
 class Summarizer:
     def __init__(self, config: dict, model_id: str ='Falconsai/text_summarization') -> None:
         self.config = config
+        if self.config.get('chunk_overlap', None) is None:
+            self.config['chunk_overlap'] = 20
+        if self.config.get('chunk_size', None) is None:
+            self.config['chunk_size'] = 1024
         self.model_id = model_id
-        self.pipeline = transformers.pipeline(
+        pipeline = transformers.pipeline(
             'summarization',
             model=model_id,
             torch_dtype=torch.bfloat16,
             device_map="auto",
             max_length=config['max_length'],
             min_length=config['min_length'],
-            do_sample=False
+            do_sample=False,
         )
+        self.pipeline = HuggingFacePipeline(pipeline=pipeline)
 
     def generate(self, text: str) -> str:
-        return self.pipeline(text)[0]['summary_text']
+        chunks = CharacterTextSplitter(separator=' ',
+            chunk_size=self.config['chunk_size'], chunk_overlap=self.config['chunk_overlap']).split_text(text)
+        print(chunks)
+        return ' '.join([self.pipeline(chunk)[0]['summary_text'] for chunk in chunks])
 
+if __name__ == '__main__':
+    test = Summarizer({'max_length': 512, 'min_length': 128}).generate('this is a test')
